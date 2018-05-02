c08bc51ca235b0c82dacb71db181711afabd41bd
萌萌的deft <1395110441@qq.com>
2018-05-02 16:31:20 +0800

    修改路由跳转的方式

---
+++ index.js
@@ -0,0 +1,67 @@
+// import Hello from '../components/Hello';
+export default [
+  {
+    desc: '',
+    path: '',
+    name: 'demo',
+    // redirect: '/cnode/content/good',
+    component (resolve) {
+      // require(['components/modules/governor/main.vue'], resolve);
+      require.ensure([], () => resolve(require('components/main.vue')), 'demo');
+    },
+    // meta: {requiresAuth: true, dynamicMenus: true},
+    children: [
+      {
+        desc: '业务模块',
+        path: 'cnode',
+        name: 'demo.cnode',
+        component (resolve) {
+          // require(['components/modules/governor/main.vue'], resolve);
+          require.ensure([], () => resolve(require('components/layout.vue')), 'demo.cnode');
+        },
+        children: [
+          // {
+          //   desc: '精华',
+          //   path: 'content/:type',
+          //   name: 'demo.cnode.content',
+          //   component (resolve) {
+          //     require.ensure([], () => resolve(require('components/modules/content.vue')), 'demo.cnode.content');
+          //   }
+          // },
+          {
+            desc: '精华',
+            path: 'content',
+            name: 'demo.cnode.content',
+            component (resolve) {
+              require.ensure([], () => resolve(require('components/modules/content.vue')), 'demo.cnode.content');
+            }
+          },
+          {
+            desc: '详情',
+            path: 'detail',
+            name: 'demo.cnode.detail',
+            component (resolve) {
+              require.ensure([], () => resolve(require('components/modules/detail.vue')), 'demo.cnode.detail');
+            }
+          },
+          {
+            desc: '用户信息',
+            path: 'userInfo',
+            name: 'demo.cnode.userInfo',
+            component (resolve) {
+              require.ensure([], () => resolve(require('components/modules/user-info.vue')), 'demo.cnode.user-info');
+            }
+          },
+          {
+            desc: '新增主题',
+            path: 'addTopic',
+            name: 'demo.cnode.addTopic',
+            component (resolve) {
+              require.ensure([], () => resolve(require('components/modules/add-topic.vue')), 'demo.cnode.add-topic');
+            }
+          }
+        ]
+      }
+    ]
+  }
+];
